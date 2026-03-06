136a9cf1ea185fe4f051684cf52c7358be9eda44
Tereams <106360504+Tereams@users.noreply.github.com>
2026-03-06 12:48:57 -0600

    fix bug: task

--- core/task.py
+++ core/task.py
@@ -14,7 +14,7 @@ class Task:
     estimate_hours: float
     priority: Optional[int] = None
 
-    def __post_init_(self):
+    def __post_init__(self):
         if self.window_start >= self.window_end:
             raise ValueError("window_start must be earlier than window_end")
 
